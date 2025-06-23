fc2adae790e3888a3d2ff9df8b367247dd28e1d0
zxhtom <870775401@qq.com>
2025-06-23 21:50:57 +0800

    add chat && lazy load keyboard settings

--- neovim/lua/utils/import.lua
+++ neovim/lua/utils/import.lua
@@ -1,16 +1,15 @@
-  local M ={}
-  M.setup = function(module)
-    require("setup."..module)
-  end
-  M.keyboards = function(module)
-    require("keyboards."..module)
-  end
-  M.config = function(module)
-    require("config."..module)
-  end
-  M.all = function(module)
-    M.setup(module)
-    M.config(module)
-    M.keyboards(module)
-  end
-  return M
+local M = {}
+M.setup = function(module)
+  pcall(require, "setup." .. module)
+end
+M.keyboards = function(module)
+  pcall(require, "keyboards." .. module)
+end
+M.config = function(module)
+  pcall(require, "config." .. module)
+end
+M.all = function(module)
+  M.setup(module)
+  M.config(module)
+end
+return M
