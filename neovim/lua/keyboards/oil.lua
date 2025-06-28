c4f01978382617d4002621c49fca7ea15a36f5a3
zxhtom <870775401@qq.com>
2025-06-28 19:36:56 +0800

    add keymaps jump to where you open initial

--- neovim/lua/keyboards/oil.lua
+++ neovim/lua/keyboards/oil.lua
@@ -1,3 +1,7 @@
+-- 快捷键跳回最初的打开目录
+vim.keymap.set("n", "<leader>ob", function()
+  require("oil").open(_G.NVIM_INITIAL_PATH)
+end, { desc = "Oil: back to nvim entry path" })
 -- 绑定 <leader>e 打开/关闭 Oil
 vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Toggle Oil file explorer" })
 
