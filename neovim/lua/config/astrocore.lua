c1209baa0c67f4b351f8143318c067fd83e99285
zxhtom <870775401@qq.com>
2025-07-01 08:38:18 +0800

    try none-ls provide format function

--- neovim/lua/config/astrocore.lua
+++ neovim/lua/config/astrocore.lua
@@ -1,13 +1,13 @@
-local loader = require "utils.config_loader"
+local loader = require("utils.config_loader")
 local opts, loaded_keyboards = loader.load_configs("keyboards")
-  -- 显式传递正确的 bufnr
-astrocore = require("astrocore")
+-- 显式传递正确的 bufnr
+local astrocore = require("astrocore")
 if astrocore then
-  astrocore.setup({
-    opts=opts,
-    on_attach = function(client, bufnr)
-      vim.diagnostic.enable(bufnr)  -- 确保 bufnr 是数字
-    end,
-  })
+	astrocore.setup({
+		opts = opts,
+		on_attach = function(client, bufnr)
+			vim.diagnostic.enable(bufnr) -- 确保 bufnr 是数字
+		end,
+	})
 end
 return opts
